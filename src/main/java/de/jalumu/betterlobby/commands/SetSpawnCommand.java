bbd489bf5bb7b3af947af72b5c6c95c13a9da96e
Fruxz <cedricspitzer@outlook.de>
2020-03-17 16:12:32 +0100

    Revised SetSpawn command

--- src/main/java/de/jalumu/betterlobby/commands/SetSpawnCommand.java
+++ src/main/java/de/jalumu/betterlobby/commands/SetSpawnCommand.java
@@ -1,10 +1,20 @@
 package de.jalumu.betterlobby.commands;
 
 import de.jalumu.betterlobby.BetterLobby;
+import de.jalumu.betterlobby.visual.Transmission;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+
+/**
+ * This is a command, that set a new spawn-point location
+ *
+ * Edited/Revised by Fruxz
+ * @version 2.0
+ */
 
 public class SetSpawnCommand implements CommandExecutor {
     @Override
@@ -12,15 +22,17 @@ public class SetSpawnCommand implements CommandExecutor {
 
         if (sender instanceof Player) {
             Player player = (Player) sender;
+            Location location = player.getLocation();
 
-            BetterLobby.getConfiguration().set("location.spawn", player.getLocation());
+            BetterLobby.getConfiguration().set("location.spawn", location);
             BetterLobby.getConfiguration().set("join.teleportToSpawn", true);
             BetterLobby.saveConfiguration();
+            player.getWorld().setSpawnLocation(location.getBlockX(), location.getBlockY(), location.getBlockZ());
 
-        } else {
-            sender.sendMessage("You are not a Player");
-            return true;
-        }
+            new Transmission(ChatColor.GREEN + "The new spawn point could be set successfully!").send(player);
+
+        } else
+            new Transmission(ChatColor.RED + "You are not a player!").send(sender);
 
         return false;
     }

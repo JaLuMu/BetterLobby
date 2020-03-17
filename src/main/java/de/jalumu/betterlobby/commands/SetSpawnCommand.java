4f49e36eb0d1edf033d5a56964738fe5b7bc870c
Fruxz <cedricspitzer@outlook.de>
2020-03-17 16:00:53 +0100

    Rearranged SetSpawnCommand.class

--- src/main/java/de/jalumu/betterlobby/commands/SetSpawnCommand.java
+++ src/main/java/de/jalumu/betterlobby/commands/SetSpawnCommand.java
@@ -10,14 +10,14 @@ public class SetSpawnCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
-        if (sender instanceof Player){
+        if (sender instanceof Player) {
             Player player = (Player) sender;
 
-            BetterLobby.getConfiguration().set("location.spawn",player.getLocation());
-            BetterLobby.getConfiguration().set("join.teleportToSpawn",true);
+            BetterLobby.getConfiguration().set("location.spawn", player.getLocation());
+            BetterLobby.getConfiguration().set("join.teleportToSpawn", true);
             BetterLobby.saveConfiguration();
 
-        }else {
+        } else {
             sender.sendMessage("You are not a Player");
             return true;
         }
